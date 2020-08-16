460fe7ff93632e5dd9e3e9d4b95ccfdcd1eaad21
Chris Williams <diodesign@tuta.io>
2020-08-16 03:51:17 -0700

    Provide common routines for access Qemu's RISC-V virt serial port for debugging purposes

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # qemuprint
-Print to Qemu's RISC-V virt serial port
+
+This library prints text to the [Qemu](https://www.qemu.org/) emulator's serial port on the `virt` RISC-V machine target. This crate is used by [Diosix](https://diosix.org) for debugging during system startup. It can also be used by non-std libraries that need to output debugging and diagnostic information during development.
+
+If you wish to use this for your own project, let me know and I'll tidy up the documentation and API.
+
+### Contact and code of conduct <a name="contact"></a>
+
+Please [email](mailto:diosix@tuta.io) Diosix project lead Chris Williams if you have any questions or issues to raise, wish to get involved, have source to contribute, or have found a security flaw. You can, of course, submit pull requests or raise issues via GitHub, though please consider disclosing security-related matters privately. Please also observe the Diosix project's [code of conduct](https://diosix.org/docs/conduct.html) if you wish to participate.
+
+### Copyright and license <a name="copyright"></a>
+
+Copyright &copy; Chris Williams, 2020. See [LICENSE](LICENSE) for distribution and use of source code and binaries.
---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+/* Print text to the serial port on RISC-V Qemu-emulated virt machines 
+ *
+ * If you need to debug low-level, non-std RISC-V code, fire this up
+ * in a Qemu virt machine, and print to the serial port, which will
+ * appear in the terminal if you use the -nographic Qemu switch. For example:
+ * 
+ *  qemu-system-riscv64 -bios none -nographic -machine virt -kernel <path to kernel>
+ * 
+ * (c) Chris Williams, 2020.
+ *
+ * See LICENSE for usage and copying.
+ */
+
+#![no_std]
+use core::fmt;
+
+const QemuVirtSerialPort: usize = 0x10000000;
+
+#[macro_export]
+macro_rules! println
+{
+    ($fmt:expr) => ($crate::print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::print!(concat!($fmt, "\n"), $($arg)*));
+}
+
+#[macro_export]
+macro_rules! print
+{
+    ($($arg:tt)*) =>
+    ({
+        use core::fmt::Write;
+        {
+            unsafe { $crate::serial::QEMUUART.write_fmt(format_args!($($arg)*)).unwrap(); }
+        }
+    });
+}
+
+pub struct UartWriter;
+pub static mut QEMUUART: UartWriter = UartWriter {};
+
+impl fmt::Write for UartWriter
+{
+    fn write_str(&mut self, s: &str) -> fmt::Result
+    {
+        for c in s.bytes()
+        {
+            unsafe { *(QemuVirtSerialPort as *mut u8) = c };
+        }
+        Ok(())
+    }
+}
